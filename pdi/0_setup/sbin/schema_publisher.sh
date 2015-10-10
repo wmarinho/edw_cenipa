712aef360129013dd7958bb39329bc2bd0255b8c
Wellington Marinho <wpmarinho@globo.com>
2015-10-10 20:10:05 +0000

    .

--- pdi/0_setup/sbin/schema_publisher.sh
+++ pdi/0_setup/sbin/schema_publisher.sh
@@ -5,6 +5,7 @@ host="edw_biserver"
 login="admin"
 password="password"
 port="8080"
-schema_path"/opt/edw_cenipa/pdi/3_schema/cenipa/cenipa.mondrian.xml"
+schema_path="/opt/edw_cenipa/3_schema/cenipa/cenipa.mondrian.xml"
 
-cmd_publisher=$(curl -v -include --user ${admin}:${password} -F "catalogName=${catalogo}" -F "overwrite=true"  -F "xmlaEnabledFlag=true" -F "parameters=DataSource=${ds}" -F "uploadAnalysis=@${schema_path}" http://${host}:${port}/pentaho/plugin/data-access/api/mondrian/postAnalysis	
+cmd_publisher=$(curl -v -include --user ${login}:${password} -F "catalogName=${catalogo}" -F "overwrite=true"  -F "xmlaEnabledFlag=true" -F "parameters=DataSource=${ds}" -F "uploadAnalysis=@${schema_path}" http://${host}:${port}/pentaho/plugin/data-access/api/mondrian/postAnalysis)
+echo ${cmd_publisher}
