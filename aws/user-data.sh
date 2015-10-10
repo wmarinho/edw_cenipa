1f74dcf2bad4331ecc3e8f72acd60e21e9d71193
Wellington Marinho <wpmarinho@globo.com>
2015-10-10 23:55:46 +0000

    .

--- aws/user-data.sh
+++ aws/user-data.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -e -x
 
-sudo yum update -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo yum install -y git
+yum update -y
+yum install -y docker
+service docker start
+usermod -a -G docker ec2-user
+yum install -y git
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > docker-compose
-sudo mv docker-compose /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 
+pip install -U docker-compose
 git clone https://github.com/wmarinho/edw_cenipa.git
 cd edw_cenipa
 sh ./install.sh
