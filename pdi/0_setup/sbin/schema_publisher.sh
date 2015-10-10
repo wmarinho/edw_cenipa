dd40cc521051c4b7daccc37f9e2be6c9149f4aea
Wellington Marinho <wmarinho@globosat.com.br>
2015-10-10 16:30:25 -0300

    .

---
+++ pdi/0_setup/sbin/schema_publisher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+catalogo="Ocorrencias Aeronauticas"
+ds="edw_cenipa"
+host="edw_biserver"
+login="admin"
+password="password"
+port="8080"
+schema_path"/opt/edw_cenipa/pdi/3_schema/cenipa/cenipa.mondrian.xml"
+
+cmd_publisher=$(curl -v -include --user ${admin}:${password} -F "catalogName=${catalogo}" -F "overwrite=true"  -F "xmlaEnabledFlag=true" -F "parameters=DataSource=${ds}" -F "uploadAnalysis=@${schema_path}" http://${host}:${port}/pentaho/plugin/data-access/api/mondrian/postAnalysis	
